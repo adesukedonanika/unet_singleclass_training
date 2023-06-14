4e88ccc39fb5be462441fb71469f49f51f7845de
adesukedonanika <adesukedonanika@gmail.com>
2023-06-15 08:57:30 +0900

    batchsize64は重すぎた。サイズ256，Batch32でFix

--- 04_segment_model_training.bat
+++ 04_segment_model_training.bat
@@ -1,9 +1,4 @@
-python forest_UAV_U_Net_pytorch.py cypress 200 64 256
+@REM python forest_UAV_U_Net_pytorch.py cypress 200 64 256
 python forest_UAV_U_Net_pytorch.py cypress 200 32 256
-python forest_UAV_U_Net_pytorch.py cypress 200 24 256
-python forest_UAV_U_Net_pytorch.py cypress 200 16 256
 
-python forest_UAV_U_Net_pytorch.py cedar 200 64 256
 python forest_UAV_U_Net_pytorch.py cedar 200 32 256
-python forest_UAV_U_Net_pytorch.py cedar 200 24 256
-python forest_UAV_U_Net_pytorch.py cedar 200 16 256
