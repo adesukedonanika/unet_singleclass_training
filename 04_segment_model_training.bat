8564f48321b5a82afdc3c316e967e90be1a57b99
adesukedonanika <adesukedonanika@gmail.com>
2023-06-14 19:09:03 +0900

    256Pixelでは成功する。Batch数を調整
    batch2 size512 is NG
    batch16 size256 is OK

--- 04_segment_model_training.bat
+++ 04_segment_model_training.bat
@@ -1,6 +1,9 @@
-python forest_UAV_U_Net_pytorch.py cypress 30 2 512
-python forest_UAV_U_Net_pytorch.py cypress 30 16 256
-python forest_UAV_U_Net_pytorch.py cypress 30 8 256
-python forest_UAV_U_Net_pytorch.py cypress 30 4 256
+python forest_UAV_U_Net_pytorch.py cypress 200 64 256
+python forest_UAV_U_Net_pytorch.py cypress 200 32 256
+python forest_UAV_U_Net_pytorch.py cypress 200 24 256
+python forest_UAV_U_Net_pytorch.py cypress 200 16 256
 
-@REM python forest_UAV_U_Net_pytorch.py cedar 200
+python forest_UAV_U_Net_pytorch.py cedar 200 64 256
+python forest_UAV_U_Net_pytorch.py cedar 200 32 256
+python forest_UAV_U_Net_pytorch.py cedar 200 24 256
+python forest_UAV_U_Net_pytorch.py cedar 200 16 256
